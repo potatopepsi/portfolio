736277357fcdda1f8cfa1507cbc59b7e7da5bf2f
potatopepsi <137965266+potatopepsi@users.noreply.github.com>
2023-06-28 23:22:26 -0400

    First Commit
    Finished setting up template
    
    Started figuring out the three different themes for the site

---
+++ README.md
@@ -0,0 +1,3 @@
+# portfolio
+
+This is my repository showing my website portfolio I made in June 2023.
---
+++ script.js
@@ -0,0 +1,49 @@
+function toggleTheme(value) {
+    document.getElementsByTagName('link').href = value;
+}
+
+function toggleHidden(){
+
+    // document.getElementById('hidden').style.visibility ='hidden';
+    // document.getElementById('hidden2').style.visibility ='hidden';
+    // document.getElementById('sight').style.visibility ='visible';
+    // document.getElementById('sight2').style.visibility ='visible';
+    document.getElementById('btn').style.visibility ='hidden';
+    document.getElementById('btn1').style.visibility ='hidden';
+    document.getElementById('btn2').style.visibility ='hidden';
+    document.getElementById('btn3').style.visibility ='hidden';
+    document.getElementById('btn4').style.visibility ='hidden';
+    document.getElementById('btn5').style.visibility ='hidden';
+    document.getElementById('btn6').style.visibility ='hidden';
+    document.getElementById('btn7').style.visibility ='hidden';
+    document.getElementById('btn8').style.visibility ='hidden';
+    document.getElementById('btn9').style.visibility ='hidden';
+    document.getElementById('btn10').style.visibility ='hidden';
+    document.getElementById('btn11').style.visibility ='hidden';
+    document.getElementById('btn12').style.visibility ='hidden';
+    document.getElementById('btn13').style.visibility ='hidden';
+
+    
+
+}
+function toggleSight(){
+
+    // document.getElementById('hidden').style.visibility ='visible';
+    // document.getElementById('hidden2').style.visibility ='visible';
+    // document.getElementById('sight').style.visibility ='hidden';
+    // document.getElementById('sight2').style.visibility ='hidden';
+    document.getElementById('btn').style.visibility ='visible';
+    document.getElementById('btn1').style.visibility ='visible';
+    document.getElementById('btn2').style.visibility ='visible';
+    document.getElementById('btn3').style.visibility ='visible';
+    document.getElementById('btn4').style.visibility ='visible';
+    document.getElementById('btn5').style.visibility ='visible';
+    document.getElementById('btn6').style.visibility ='visible';
+    document.getElementById('btn7').style.visibility ='visible';
+    document.getElementById('btn8').style.visibility ='visible';
+    document.getElementById('btn9').style.visibility ='visible';
+    document.getElementById('btn10').style.visibility ='visible';
+    document.getElementById('btn11').style.visibility ='visible';
+    document.getElementById('btn12').style.visibility ='visible';
+    document.getElementById('btn13').style.visibility ='visible';
+}
